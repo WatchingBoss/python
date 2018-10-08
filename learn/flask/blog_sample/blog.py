9068c2654a4ca7bc560c52fad8a1c266b27ae9d2
Watchingboss <opil94@yandex.ru>
2018-10-08 19:10:38 +0300

    Blog sample

--- learn/flask/blog_sample/blog.py
+++ learn/flask/blog_sample/blog.py
@@ -1,13 +1,22 @@
 import flask
 app = flask.Flask(__name__)
 
-
-@app.route("/about")
-def about():
-    return flask.render_template("about.html")
+items = [
+    {"header": "This is first post",
+     "first": "Here is half of text",
+     "second": "And second part of text"},
+    {"header": "This is second post",
+     "first": "Here is half of text",
+     "second": "And second part of text"}
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return flask.render_template("home.html")
+    return flask.render_template("home.djhtml", posts=items, title="Home")
+
+
+@app.route("/about")
+def about():
+    return flask.render_template("about.djhtml", title="About")
---
+++ learn/flask/blog_sample/flask.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=blog.py
+export FLASK_ENV=development
+flask run
